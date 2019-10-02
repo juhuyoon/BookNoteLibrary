92a9b7cd1cc78517f8a514b45b4a2b12893fc48d
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-02 14:35:17 -0400

    Created Test File with Aaron who is a boss!

---
+++ note-service/src/test/java/com/example/noteservice/dao/NoteDaoTest.java
@@ -0,0 +1,37 @@
+package com.example.noteservice.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NoteDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void addNote() {
+    }
+
+    @Test
+    public void getNote() {
+    }
+
+    @Test
+    public void getAllNotes() {
+    }
+
+    @Test
+    public void updateNote() {
+    }
+
+    @Test
+    public void deleteNote() {
+    }
+
+    @Test
+    public void getNotesByBookId() {
+    }
+}
