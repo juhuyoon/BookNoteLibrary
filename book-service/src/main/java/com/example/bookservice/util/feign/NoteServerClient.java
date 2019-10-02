1a547926e0fc2c17ab1b76ecb0c9514195142cd9
Junghoon Yoon <juhuyoon@yahoo.com>
2019-10-02 15:17:11 -0400

    adding bookservicelayer(wip) and noteserverclient update

--- book-service/src/main/java/com/example/bookservice/util/feign/NoteServerClient.java
+++ book-service/src/main/java/com/example/bookservice/util/feign/NoteServerClient.java
@@ -2,16 +2,22 @@ package com.example.bookservice.util.feign;
 
 import com.example.bookservice.dto.Note;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @FeignClient("note-service")
 public interface NoteServerClient {
-    @GetMapping(value = "/notes/{id}")
-    public List getNotesByBook();
+
+    @GetMapping(value = "/notes/book/{book_id}")
+    public List getNotesByBook(@PathVariable int book_id);
 
     @PostMapping(value = "/notes")
-    public Object postNotes(Note note);
+    public Note postNotes(Note note);
+
+    @PutMapping(value = "/{id}")
+    public Note updateNotes(@PathVariable int id);
+
+    @DeleteMapping("{id}")
+    public void deleteNotes(@PathVariable int id);
 }
