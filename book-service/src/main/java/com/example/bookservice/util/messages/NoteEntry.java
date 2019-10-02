1c345f3e6616065360c537a122577042e6d541c4
Asteria <dcdion22@gmail.com>
2019-10-02 15:50:48 -0400

    Controllers

---
+++ book-service/src/main/java/com/example/bookservice/util/messages/NoteEntry.java
@@ -0,0 +1,49 @@
+//package com.example.bookservice.util.messages;
+//
+//import com.example.bookservice.dto.Note;
+//
+//public class NoteEntry extends Note {
+//
+//    public NoteEntry() {
+//        super();
+//    }
+//
+//    public NoteEntry(int noteId, int bookId, String note) {
+//        super(noteId, bookId, note);
+//    }
+//
+//    @Override
+//    public int getNoteId() {
+//        return super.getNoteId();
+//    }
+//
+//    @Override
+//    public void setNoteId(int noteId) {
+//        super.setNoteId(noteId);
+//    }
+//
+//    @Override
+//    public int getBookId() {
+//        return super.getBookId();
+//    }
+//
+//    @Override
+//    public void setBookId(int bookId) {
+//        super.setBookId(bookId);
+//    }
+//
+//    @Override
+//    public String getNote() {
+//        return super.getNote();
+//    }
+//
+//    @Override
+//    public void setNote(String note) {
+//        super.setNote(note);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "NoteEntry{}";
+//    }
+//}
