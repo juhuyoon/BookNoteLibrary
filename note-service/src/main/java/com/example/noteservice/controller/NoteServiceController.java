467f4da85aeed900e0fd136ae56461bcf43249b7
DevAaron <devaaronlee@gmail.com>
2019-10-03 14:42:16 -0400

    add note queue consumer

--- note-service/src/main/java/com/example/noteservice/controller/NoteServiceController.java
+++ note-service/src/main/java/com/example/noteservice/controller/NoteServiceController.java
@@ -3,6 +3,7 @@ package com.example.noteservice.controller;
 import com.example.noteservice.dao.NoteDao;
 import com.example.noteservice.dto.Note;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import java.util.List;
 @RefreshScope
 public class NoteServiceController {
 
+    @Qualifier("noteDao")
     @Autowired
     NoteDao noteDao;
 
